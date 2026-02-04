d4d82faea5ec8f1300bffea1fcb4a6baea9114e6
kazhymukanboris-dev <kazhymukanboris@gmail.com>
2026-02-04 22:48:57 +0500

    admin.py

--- reviews/admin.py
+++ reviews/admin.py
@@ -1,14 +1,18 @@
 from django.contrib import admin
 from .models import MediaItem, Review
 
+
 @admin.register(MediaItem)
 class MediaItemAdmin(admin.ModelAdmin):
-    list_display = ('title', 'category', 'author_or_director', 'release_year')
-    list_filter = ('category', 'release_year')
-    search_fields = ('title', 'author_or_director')
+    list_display = ("id", "title", "category", "author_or_director", "release_year")
+    list_filter = ("category", "release_year")
+    search_fields = ("title", "author_or_director", "description")
+    ordering = ("-release_year", "title")
+
 
 @admin.register(Review)
 class ReviewAdmin(admin.ModelAdmin):
-    list_display = ('item', 'user', 'rating', 'created_at')
-    list_filter = ('rating', 'created_at')
-    search_fields = ('comment', 'user__username', 'item__title')
+    list_display = ("id", "item", "user", "rating", "created_at")
+    list_filter = ("rating", "created_at")
+    search_fields = ("comment", "user__username", "item__title")
+    ordering = ("-created_at",)
